eac8e882fa858c25468d87737b9a824562bc4ad2
Carter3190 <csabes3190@gmail.com>
2025-04-01 19:54:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+<script>
+    
+document.getElementById('signInText').addEventListener('click', function() {
+    window.location.href = 'sign-in.html';  // Change to your actual sign-in page
+});
+</script>
